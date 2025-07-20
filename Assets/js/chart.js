060bc33bfe2cac6032cad9296321190b906b02c7
apeditz07 <apeditz07@gmail.com>
2025-07-20 16:34:46 +0530

    Delete Assets directory

--- Assets/js/chart.js
+++
@@ -1,7 +0,0 @@
-function loadChart() {
-  const symbol = document.getElementById("symbolSelector").value;
-  document.getElementById("tradingview-widget").innerHTML = `Chart for ${symbol}`;
-}
-function toggleTheme() {
-  document.body.classList.toggle('dark');
-}
